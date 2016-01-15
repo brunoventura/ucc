6ae73e0f23a8bbb70ed40ac79717144991960c37
Bruno Ventura <brun0ventur4@gmail.com>
2016-01-15 09:56:00 -0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require( 'gulp' );
+var connect = require( 'gulp-connect' );
+var files = ['lp/**/*'];
+
+gulp.task( 'files', function() {
+  gulp.src( files ).pipe( connect.reload() );
+});
+
+gulp.task( 'watch', function() {
+  gulp.watch( files, [ 'files' ]);
+});
+
+gulp.task( 'connect', function() {
+  connect.server({ livereload: true });
+});
+
+gulp.task( 'default', [ 'connect', 'watch' ]);
